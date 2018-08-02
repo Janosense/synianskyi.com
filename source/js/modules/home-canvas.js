9c9d7d4fc67f99945e8067827fd285681e05b3a9
Janosense <janosense@gmail.com>
2018-08-02 19:09:32 +0300

    Промежуточный коммит.

--- source/js/modules/home-canvas.js
+++ source/js/modules/home-canvas.js
@@ -130,7 +130,7 @@
 
       balls.forEach(function(ball) {
         think(mousePosition, ball);
-        drawSquare(ball.x, ball.y, 20, 20, '#d0d0d0');
+        drawSquare(ball.x, ball.y, 20, 20, 'rgba(208, 208, 208, 0.7)');
       });
     };
 
